5972689b164353eca62ec0e2f0ba3234048b74ce
zubairadams <mfundo8934@gmail.com>
2019-12-06 16:27:51 +0200

    added radio buttons and text bpx

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -18,20 +18,44 @@ public class HelloWorld {
 
         }, new HandlebarsTemplateEngine());
 
-        get("/hello/:name", (req, res) -> {
+        get("/", (req,res) -> {
+
             Map<String, String> dataMap = new HashMap<>();
-            dataMap.put("name", ":name");
+            return new ModelAndView(dataMap, "greets.hbs");
+        },  new HandlebarsTemplateEngine());
+
+        post("/greet", (req, res) -> {
+
+            String name = req.queryParams("firstname");
+            String language = req.queryParams("optradio");
+
+
+            Map<String, String> dataMap = new HashMap<>();
+            dataMap.put("name", greet(name, language));
             return new ModelAndView(dataMap, "hello.hbs");
         }, new HandlebarsTemplateEngine());
 
 //      initExceptionHandler((e) -> System.out.println("Uh-oh"));
 
     }
-    static int getHerokuAssignedPort() {
+     static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get("PORT") != null) {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
         return 4567;
+    }
+    public static String greet(String name, String language) {
+        String msg = "Invalid Selection";
+         if(language.equals("English") & name.length() > 0){
+            msg = "Hello " + name;
+             System.out.println(msg);
+        }else if(language.equals("Xhosa") & name.length() > 0){
+            msg = "Molo " + name;
+        }else if(language.equals("Mandarin") & name.length() > 0) {
+            msg = "Ni hao " + name;
+
+        }
+        return msg;
     }
 }
